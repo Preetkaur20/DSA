2d5f09d38cfd912c2847034d2a458fc671ddf6d9
Preetkaur Relusinghani <99186601+Preetkaur20@users.noreply.github.com>
2023-07-18 18:32:24 +0530

    Update array.java

--- array.java
+++ array.java
@@ -1,3 +1,5 @@
+// Array - a collection of similar types data type.
+
 public class array {
     public static void main(String[] args) {
         int[][] marks = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
